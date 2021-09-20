6a12b4d026d1e8caacf6f8d593e48f7224399593
Jerry <1364812552@qq.com>
2021-09-20 01:06:53 -0700

    update test code

---
+++ test/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gcc hello_world_server.c -o hello_world_server -levent
+
+gcc hello_world_client.c -o hello_world_client -levent
+
---
+++ test/hello_world_client.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+static const int PORT = 9995;
+
+int main(int argc, char* argv[]) {
+    printf("say hello\n");
+    int sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sfd < 0) {
+        printf("create socket failed!\n");
+        goto error;
+    }
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    //addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    if (connect(sfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        printf("connect failed!\n");
+        goto error;
+    }
+
+    char recvbuf[BUFFER_SIZE] = {0};
+    if (recv(sfd, recvbuf, BUFFER_SIZE, 0) < 0) {
+        printf("recv error!\n");
+        goto error;
+    }
+    printf("recv data: %s\n", recvbuf);
+
+error:
+    close(sfd);
+    return 0;
+}
